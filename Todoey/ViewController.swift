5408640a51533befa56d14c068a6b41afccd68d5
Ali Khansari <ali@wencelworldwide.com>
2019-06-13 15:33:10 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ali Khansari on 6/13/19.
+//  Copyright © 2019 Web Consultant 247, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
